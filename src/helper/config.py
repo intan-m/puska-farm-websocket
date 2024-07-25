6b81973b8493a42dd8c8433cd74f6608e2ddde1b
Leonardi Fabianto <29391835+avidito@users.noreply.github.com>
2024-07-25 09:55:37 +0700

    feat: adding cert (#3)

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,5 +5,6 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     API_HOSTNAME: str = Field(alias="API_HOSTNAME")
     API_PORT: int = Field(alias="API_PORT")
+    CERT_DIR: str = Field(alias="CERT_DIR")
 
 CONFIG = Config()
